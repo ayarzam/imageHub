f9611e147400ded84ce382ca71e6ef3a6d81d458
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-09-06 00:38:48 -0400

    finally got the single image working with the databaase, working on adding multiple images to the database next

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      file: null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.resetForm = this.resetForm.bind(this)
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const data = new FormData();
+      data.append('photo', this.state.file);
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      };
+    const response = await axios.post('/api/add', data, config)
+    alert("You've successfully uploade your file")
+    this.resetForm()
+    } catch (error) {
+      console.log('Error in the React component')
+    }
+  }
+  resetForm(){
+    this.setState({file: null})
+  }
 
-export default App;
+  onChange(event){
+    this.setState({file: event.target.files[0]})
+  }
+
+  render(){
+    console.log(this.state)
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor='file'>File</label>
+            <input type='file' name="photo" onChange={this.onChange}/>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+
+};
